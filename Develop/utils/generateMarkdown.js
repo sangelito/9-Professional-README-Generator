f9c118ea013f81d5d02c2033c995b887ad122246
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-11 14:09:50 -0500

    generate markdown function

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -12,7 +12,35 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# Project Title
+   ${data.title}
+   ${data.getLicense}
+
+   # Description
+   ${data.description}
+
+   # Table of Contents 
+* [Installation](#-Installation)
+* [Usage](#-Usage)
+* [License](#-Installation)
+* [Contributing](#-Contributing)
+* [Tests](#-Tests)
+* [Questions](#-Contact-Information)
+    
+# Installation
+${data.installation}
+# Usage
+${data.usage}
+# License 
+${data.license}
+* As this license list was not comprehensive, if you need another license, use the contact information below to ask for license to be added. 
+# Contributing 
+${data.contributing}
+# Tests
+${data.tests}
+# Contact Information 
+* GitHub Username: ${data.userName}
+* Contact Email: ${data.userEmail}
 
 `;
 }
