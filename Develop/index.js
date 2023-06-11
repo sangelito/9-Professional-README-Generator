739ec0b08332e7d3a1095cf3fca4b198aa74fe3d
sangelito <129316135+sangelito@users.noreply.github.com>
2023-06-11 14:52:24 -0500

    write to file readme

--- Develop/index.js
+++ Develop/index.js
@@ -118,7 +118,13 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {
